a6c660969d32242b61737741aa25e89141b28ca3
Pasot01 <contactmyphone14@gmail.com>
2022-08-07 18:26:44 +0200

    Config var .env pour le port, middleware et routes basiques

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+require('dotenv').config()
 const express = require('express');
 
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// routes
+app.get('/', (req, res) => {
+    res.json({ mesg: 'Welcome to the app' });
+});
+
 // listen for requests
-app.listen(4000, () => {
-    console.log('Listening on port 4000');
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT);
 });
