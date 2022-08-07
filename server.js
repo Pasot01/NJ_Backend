7c8c46895b17d9afa7aa9fd2913681e85b56f964
Pasot01 <contactmyphone14@gmail.com>
2022-08-07 21:44:39 +0200

    Ajout du module Mongoose et configuration de la connection db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 const app = express();
@@ -16,7 +17,20 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT);
-});
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('connected to database')
+        // listen to port
+        app.listen(process.env.PORT, () => {
+            console.log('listening for requests on port', process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// listen for requests -> remplacé par mongoose
+// app.listen(process.env.PORT, () => {
+//     console.log('Listening on port', process.env.PORT);
+// });
